61c5c5639d2df99c1bc0ae47f92fd2e8b72c6a08
vhorbatiuk <vitaljiano@gmail.com>
2021-12-16 12:56:20 +0200

    changed AWS_SECRETE_NAME

--- src/test/java/com/example/time/demo/demo/integration/CustomTimeIntegrationTest.java
+++ src/test/java/com/example/time/demo/demo/integration/CustomTimeIntegrationTest.java
@@ -1,24 +1,15 @@
 package com.example.time.demo.demo.integration;
 
+import com.example.time.demo.demo.CustomTimeAbstractIntegrationTest;
 import com.example.time.demo.demo.model.CustomTime;
 import com.example.time.demo.demo.util.JsonCustomTimeParser;
 import com.example.time.demo.demo.util.ListCustomTime;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import java.util.List;
 
@@ -27,31 +18,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
 @TestPropertySource("/applicationForTest.properties")
-@AutoConfigureMockMvc
-@Sql(value = {"/set-value-time-auto-set.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
-@Sql(value = {"/clear-data-after-testing.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
-public class IntegrationTest {
-
-    @Autowired
-    private WebApplicationContext wac;
-    @Autowired
-    ObjectMapper objectMapper;
-
+public class CustomTimeIntegrationTest extends CustomTimeAbstractIntegrationTest {
     String customTimeInstance = JsonCustomTimeParser.getResourceFileAsString("test-json/CustomTimeInstance.json");
     List<CustomTime> timeList = ListCustomTime.getListCustomTime();
-    private MockMvc mockMvc;
-
-    @BeforeEach
-    public void setupMockMvc() {
-        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
-    }
+    public static final String pathUrl = "/api/v1/time";
 
     @Test
     public void searchFromDBTimeTest() throws Exception {
         RequestBuilder requestBuilder = MockMvcRequestBuilders
-                .get("/{id}", 0);
+                .get(pathUrl + "/{id}", 0);
 
         mockMvc.perform(requestBuilder)
                 .andDo(print())
@@ -67,7 +43,7 @@ public class IntegrationTest {
         String requestJson = objectMapper.writeValueAsString(timeList.get(3));
 
         RequestBuilder requestBuilder = MockMvcRequestBuilders
-                .post("/")
+                .post(pathUrl + "/")
                 .content(requestJson)
                 .contentType(MediaType.APPLICATION_JSON);
 
@@ -78,7 +54,7 @@ public class IntegrationTest {
 
     @Test
     public void shouldReturnAllRecordsFromDB() throws Exception {
-        mockMvc.perform(get("/"))
+        mockMvc.perform(get(pathUrl + "/"))
                 .andDo(print())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
