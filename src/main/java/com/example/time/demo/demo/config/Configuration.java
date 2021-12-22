c6d40f6b912817437760a4316b58df04dc99ada5
vhorbatiuk <vitaljiano@gmail.com>
2021-12-22 11:49:52 +0200

    changed config.yml: add mysql config

--- src/main/java/com/example/time/demo/demo/config/Configuration.java
+++ src/main/java/com/example/time/demo/demo/config/Configuration.java
@@ -2,18 +2,23 @@ package com.example.time.demo.demo.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Component;
 
+import javax.sql.DataSource;
+
 @Component
 public class Configuration {
-//    @Bean
-//    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
-//        ObjectMapper mapper = new ObjectMapper();
-//        mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
-//        MappingJackson2HttpMessageConverter converter =
-//                new MappingJackson2HttpMessageConverter(mapper);
-//        return converter;
-//    }
+//@Bean
+//public DataSource cardDataSource(Environment environment) {
+//   DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+//   dataSourceBuilder.driverClassName(environment.getRequiredProperty("DB_DRIVER"));
+//   dataSourceBuilder.url(environment.getRequiredProperty("CARD_DB_URL"));
+//   dataSourceBuilder.username(environment.getRequiredProperty("DB_ROOT_USER"));
+//   dataSourceBuilder.password(environment.getRequiredProperty("DB_PASS"));
+//   return dataSourceBuilder.build();
+//}
 }
