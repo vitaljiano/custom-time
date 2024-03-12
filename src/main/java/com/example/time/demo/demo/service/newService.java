711ea9fe13ea0caa48538e0081be00823986e632
vhorbatiuk <vhorbatiuk@binariks.com>
2024-03-12 13:59:06 +0200

    refactoring:add new service

---
+++ src/main/java/com/example/time/demo/demo/service/newService.java
@@ -0,0 +1,10 @@
+package com.example.time.demo.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class newService {
+    public void newFunction(){
+        System.out.println("New function");
+    }
+}
